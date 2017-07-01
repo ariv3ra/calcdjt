c90a8ac77616fb2ede36c186988db8e9dd23d516
Angel Rivera <arivera@point.io>
2017-07-01 09:44:38 -0400

    added copy line to buld file

--- build_docker.sh
+++ build_docker.sh
@@ -5,4 +5,5 @@ output=$(aws --profile datapunks ecr get-login --region us-east-1 ) && $output
 docker build --rm=true -t gustos .
 docker tag gustos:latest 146563464137.dkr.ecr.us-east-1.amazonaws.com/gustos:latest
 docker push 146563464137.dkr.ecr.us-east-1.amazonaws.com/gustos:latest
+cp config.json ~/Dropbox/stuff/creds/gustos/config.json
 echo "the docker build is complete"
