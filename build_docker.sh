5b88decbd869aee24967d2526da265d80118e332
Angel Rivera <ariv3ra@gmail.com>
2020-10-08 21:55:00 +0000

    commented stuff out

--- build_docker.sh
+++ build_docker.sh
@@ -5,5 +5,5 @@ docker build --rm=true -t gustos .
 docker tag gustos:latest 146563464137.dkr.ecr.us-east-1.amazonaws.com/gustos:latest
 output=$(aws ecr get-login --no-include-email --region us-east-1 ) && $output
 docker push 146563464137.dkr.ecr.us-east-1.amazonaws.com/gustos:latest
-cp config.json ~/Dropbox/stuff/creds/gustos/config.json
+#cp config.json ~/Dropbox/stuff/creds/gustos/config.json
 echo "the docker build is complete"
